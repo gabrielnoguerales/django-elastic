b7224279307a70e5fb8549be05ea0f30efb57732
ZC00182 <gabriel.nogueraleselviro@telefonica.com>
2021-10-11 19:09:07 +0200

    Add search and Django-Rest-Framework

--- src/productos/search.py
+++ src/productos/search.py
@@ -1,17 +1,13 @@
-from elasticsearch_dsl.query import Q, MultiMatch, SF
+from elasticsearch_dsl.query import Q, MultiMatch, SF, Range
 from .documents import ProductDocument
 
-def get_search_query(phrase):
+def get_search_products_query(phrase):
     query = Q(
-        'function_score',
-        query=MultiMatch(
-            fields=['titulo','descripcion'],
-            query=phrase
-        ),
-        functions=[
-            SF('field_value_factor', field='number_of_views')
-        ]
+        'multi_match',
+        fields=['titulo','descripcion'],
+        query=phrase
     )
     return ProductDocument.search().query(query)
-def search(phrase):
-    return get_search_query(phrase).to_queryset()
+
+def search_products(phrase):
+    return get_search_products_query(phrase).to_queryset()
